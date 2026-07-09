967993896f49e8666e769492b6abafa7d3f8ac68
wdesadwdaws <nevergivemeuphahaha@gmail.com>
2026-07-09 17:46:37 +1000

    Update Src.lua

--- Src.lua
+++ Src.lua
@@ -225,6 +225,22 @@ function lib:new(name: string, draggable: boolean, keybind: Enum.KeyCode?, theme
 		Search.TextColor3 = Color3.fromRGB(0, 0, 0)
 		Search.TextSize = 14.000
 
+		Search:GetPropertyChangedSignal("Text"):Connect(function()
+			local Search = string.lower(SearchBox.Text)
+
+			for _, Child in pairs(TabFrame:GetChildren()) do
+				if Child:IsA("Frame") or Child:IsA("TextButton") then
+					if Search == "" then
+						Child.Visible = true
+					elseif string.find(string.lower(Child.Name), Search) then
+						Child.Visible = true
+					else
+						Child.Visible = false
+					end
+				end
+			end
+		end)
+
 		Close.Name = "Close"
 		Close.Parent = Topbar
 		Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -235,6 +251,10 @@ function lib:new(name: string, draggable: boolean, keybind: Enum.KeyCode?, theme
 		Close.Size = UDim2.new(0.0666666701, 0, 0.727272749, 0)
 		Close.Image = "rbxassetid://10734896206"
 		Close.ImageColor3 = Color3.fromRGB(140, 140, 140)
+
+		Close.MouseButton1Click:Connect(function()
+        	Preview:Destroy()
+    	end)
 
 		Exit.Name = "Exit"
 		Exit.Parent = Topbar
