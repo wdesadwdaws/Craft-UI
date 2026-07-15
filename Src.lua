44323b6e510c507ed02e29bce08c82a421c66467
wdesadwdaws <nevergivemeuphahaha@gmail.com>
2026-07-15 11:43:23 +1000

    Update Src.lua

--- Src.lua
+++ Src.lua
@@ -32,6 +32,46 @@ function lib:tween(object, goal, callback, info)
 	return tween
 end
 
+local function MakeDraggable(frame)
+    if not frame then return end
+    
+    local dragging = false
+    local dragInput
+    local dragStart
+    local startPos
+
+    local function update(input)
+        local delta = input.Position - dragStart
+        frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+    end
+
+    frame.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+            dragging = true
+            dragStart = input.Position
+            startPos = frame.Position
+            
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragging = false
+                end
+            end)
+        end
+    end)
+
+    frame.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+            dragInput = input
+        end
+    end)
+
+    UserInputService.InputChanged:Connect(function(input)
+        if dragging and input == dragInput then
+            update(input)
+        end
+    end)
+end
+
 function lib:new(name: string, draggable: boolean, keybind: Enum.KeyCode?, theme: string?)
 	local GUI = {}
 	GUI.Tabs = {}
@@ -204,6 +244,20 @@ function lib:new(name: string, draggable: boolean, keybind: Enum.KeyCode?, theme
 		Line.BorderSizePixel = 0
 		Line.Position = UDim2.new(0, 0, 1, 0)
 		Line.Size = UDim2.new(1, 0, 0, 1)
+
+		if draggable then
+			MakeDraggable(Main)
+		end
+
+		if keybind then
+        	uis.InputBegan:Connect(function(input, gameProcessed)
+            	if gameProcessed then return end
+
+            	if input.KeyCode == keybind then
+               		Main.Visible = not Main.Visible
+            	end
+        	end)
+    	end
 	end
 	
 	do
